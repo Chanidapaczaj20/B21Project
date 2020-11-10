587a2f3305f63f7c73b3a9b0cf2468ba3a025f91
Chanidapaczaj20 <70244412+Chanidapaczaj20@users.noreply.github.com>
2020-11-10 12:16:51 -0500

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
-
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
         System.out.println("Hello");
+        // B21 is good at intelliJ
     }
 }
